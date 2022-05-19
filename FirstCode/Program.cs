018f191af69f800b54350088e2c318d8f54e92c8
Reginaldo Reis de Santana <regynaldoonline@hotmail.com>
2022-05-19 16:45:47 -0300

    first

---
+++ FirstCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
