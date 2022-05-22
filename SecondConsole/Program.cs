61a395bc970bf0aa56f5dbd61c42ed656a6e91f6
regymatrix <regymatrix@gmail.com>
2022-05-22 08:57:53 -0300

    Demo PEssoa

--- SecondConsole/Program.cs
+++ SecondConsole/Program.cs
@@ -1,8 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using static System.Console;
 
+public class Program{
+    public static void Main()
+    {
+        int a =2;
+        a = Adicionar20(a);
+        Console.WriteLine($"O Valor da variável é {a}");
+        // Sort mysort = new Sort();
+        // mysort.Generate();
+        // Console.WriteLine("Number sort is "+mysort.NumberSort);
+    }
+    static int Adicionar20(int a){
+        return a + 20;
+    }
 
-Sort mysort = new Sort();
-mysort.Generate();
+}
 
-Console.WriteLine("Number sort is "+mysort.NumberSort);
