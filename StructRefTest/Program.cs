2d9e315064e9e8c30ada1f6a62645643111c1535
regymatrix <regymatrix@gmail.com>
2022-05-22 09:25:26 -0300

    Demo Struct

---
+++ StructRefTest/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Ricardo";
+p1.Idade = 30;
+p1.EnderecoPessoa = new Endereco(){
+    Cidade="Estância",
+    Logradouro="Rua B",
+    Numero=300
+};
+
+WriteLine("Fim");
+
